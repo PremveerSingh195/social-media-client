08a5c3b2a89a944dc61d5d6aba4561addd1e684b
Premveer Singh <prem.veer2836@gmail.com>
2026-09-18 11:07:18 +0530

    fdsafds

---
+++ src/lib/api.ts
@@ -0,0 +1,12 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+
+export async function apiFetch(endpoint: string, options: RequestInit = {}) {
+  return fetch(`${API_URL}${endpoint}`, {
+    ...options,
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+      ...options.headers,
+    },
+  });
+}
